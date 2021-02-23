d70c32ccca9b7b82dc7d2c93df312b4ceeca9727
Thomas Chaumeny <t.chaumeny@gmail.com>
2021-02-23 15:32:56 +0100

    Refactor LoginScreen

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,35 +1,55 @@
 import * as React from 'react';
-import { TextInput, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View } from 'react-native';
+import { Button, Input } from 'react-native-elements';
 
 import { AuthContext } from '../AuthContext';
 
-export default function LoginScreen({ navigation }) {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+export default class LoginScreen extends React.Component {
+  static contextType = AuthContext;
 
-  return (
-    <AuthContext.Consumer>{ auth =>
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      errors: {}
+    };
+  }
+
+  signIn() {
+    const errors = {};
+    if (this.state.username.trim() === '') errors.username = "Please enter your username!";
+    if (this.state.password.trim() === '') errors.password = "Please enter your password!";
+    if (Object.keys(errors).length !== 0) {
+      this.setState({errors});
+      return;
+    }
+    this.context.signIn(this.state.username, this.state.password);
+    this.props.navigation.navigate('Home');
+  }
+
+  render() {
+    return (
       <View>
-        <TextInput
+        <Input
           placeholder="Username"
-          value={username}
-          onChangeText={setUsername}
+          value={this.state.username}
+          onChangeText={(username) => this.setState({username})}
+          leftIcon={{ name: 'person' }}
+          errorMessage={this.state.errors.username}
         />
-        <TextInput
+        <Input
           placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
+          value={this.state.password}
+          onChangeText={(password) => this.setState({password})}
+          leftIcon={{ name: 'lock' }}
+          errorMessage={this.state.errors.password}
           secureTextEntry
         />
         <View style={{margin:7}}>
-          <Button title="Sign in" onPress={() => {
-            auth.signIn(username, password);
-            navigation.navigate('Home');
-          }} />
+          <Button title="Sign in" onPress={this.signIn.bind(this)} />
         </View>
       </View>
-    }
-    </AuthContext.Consumer>
-  );
+    );
+  }
 }

9ddd7fa022b11373541aa96a29f765b81e6bbae5
Thomas Chaumeny <t.chaumeny@gmail.com>
2021-03-29 15:16:39 +0200

    Fix search results overlap

--- src/components/SearchScreen.js
+++ src/components/SearchScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, FlatList, Linking, Pressable, View } from 'react-native';
+import { ActivityIndicator, Linking, Pressable, ScrollView } from 'react-native';
 import { AuthContext } from '../AuthContext';
 import { Avatar, ListItem, SearchBar, Text } from 'react-native-elements';
 import theme from '../theme';
@@ -32,7 +32,7 @@ export default class SearchScreen extends React.Component {
 
   render() {
     return (
-      <View>
+      <ScrollView>
         <SearchBar
           lightTheme={true}
           clearIcon={false}
@@ -42,30 +42,24 @@ export default class SearchScreen extends React.Component {
           value={this.state.q}
         />
         { this.state.loading ? <ActivityIndicator color={theme.colors.primary} size='large'/>
-        : this.state.result ?
-        <FlatList
-          data={this.state.result.results}
-          renderItem={
-            ({item}) =>
-            <ListItem bottomDivider>
-              <Avatar
-                source={{uri: `https://img.youtube.com/vi/${item.video_id}/default.jpg`}}
-                size='medium' icon={{name: 'play-circle-filled'}}
-                onPress={() => {Linking.openURL(`https://www.youtube.com/watch?v=${item.video_id}`)}}
-              />
-              <ListItem.Content>
-                <Pressable onPress={() => this.props.navigation.navigate('Details', {video_id: item.video_id})}>
-                  <ListItem.Title>{item.name}</ListItem.Title>
-                  <ListItem.Subtitle>Score : {item.score.toFixed(0)}</ListItem.Subtitle>
-                </Pressable>
-              </ListItem.Content>
-            </ListItem>
-          }
-          keyExtractor={item => item.id.toString()}
-        />
+        : this.state.result ? this.state.result.results.map((item) =>
+          <ListItem key={item.id.toString()} bottomDivider>
+            <Avatar
+              source={{uri: `https://img.youtube.com/vi/${item.video_id}/default.jpg`}}
+              size='medium' icon={{name: 'play-circle-filled'}}
+              onPress={() => {Linking.openURL(`https://www.youtube.com/watch?v=${item.video_id}`)}}
+            />
+            <ListItem.Content>
+              <Pressable onPress={() => this.props.navigation.navigate('Details', {video_id: item.video_id})}>
+                <ListItem.Title>{item.name}</ListItem.Title>
+                <ListItem.Subtitle>Score : {item.score.toFixed(0)}</ListItem.Subtitle>
+              </Pressable>
+            </ListItem.Content>
+          </ListItem>
+        )
         : <Text>Veuillez saisir votre recherche.</Text>
         }
-      </View>
+      </ScrollView>
     )
   }
 }
